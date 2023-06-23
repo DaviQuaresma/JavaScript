401a08bb5e39deb6d7a761ab00c0350f01ab783f
Davi Quaresma da Silva Henriques <quaresma.davi@gmail.com>
2023-06-23 13:55:08 -0300

    dayjs

---
+++ AulasJavaScript III/exercicio16-datasComDayjs/day.js
@@ -0,0 +1,13 @@
+const dayjs = require("dayjs")
+
+const birth = '2002-02-09'
+
+
+
+
+
+
+/*Você deverá criar uma função javascript que recebe como parâmetro a data de nascimento de uma pessoa no 
+formato string e mostre no terminal a idade dessa pessoa no dia atual, sua próxima data de aniversário no 
+formato “DD/MM/AAAA” e quantos dias faltam para o próximo aniversário dela. Para isso, você deverá utilizar 
+o módulo dayjs, disponível no npm.*/
